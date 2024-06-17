4bdfbaea71fcc0e39bb3d7c5d3113d5bf115f956
Sebastroyer <20030625@itesa.edu.mx>
2024-06-17 09:34:46 -0600

    Versión05. Controladores de la API completos.

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -30,5 +30,37 @@ namespace Sales.API.Controllers
         {
             return Ok(await _context.Countries.ToListAsync());
         }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetAsync(int id) 
+        { 
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null) 
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+           var country = await _context.Countries.FirstOrDefaultAsync( x => x.Id == id);
+            if (country == null) 
+            {
+                return NotFound();
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
     }
 }
